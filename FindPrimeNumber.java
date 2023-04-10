12cae04ef42ac3b982e9cf7e6209ea62bbca08f2
abhishekra07 <abhira719@gmail.com>
2023-04-10 13:52:58 +0530

    Update FindPrimeNumber.java

--- FindPrimeNumber.java
+++ FindPrimeNumber.java
@@ -1,2 +1,27 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class FindPrimeNumber {
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 20);
+        // Prime number
+        System.out.println(numbers.stream()
+                .filter(FindPrimeNumber::isPrime2)
+                .collect(Collectors.toList()));
+    }
+
+    public static boolean isPrime(int number) {
+        for (int i = 2; i <= number / 2; i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean isPrime2(int number) {
+        return IntStream.rangeClosed(2, number/2).noneMatch(i -> number%i == 0);
+    }
 }
