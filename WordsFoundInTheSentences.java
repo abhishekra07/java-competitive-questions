538318abb5736b7a019543f5ced95a5f14bdeeb8
abhishekra07 <abhira719@gmail.com>
2023-04-11 17:31:06 +0530

    Update WordsFoundInTheSentences.java

--- WordsFoundInTheSentences.java
+++ WordsFoundInTheSentences.java
@@ -1,2 +1,16 @@
 public class WordsFoundInTheSentences {
+    public static void main(String[] args) {
+        System.out.println(mostWordsFound(new String[]{"alice and bob love leetcode", "i think so too", "this is great thanks very much"}));
+    }
+
+    public static int mostWordsFound(String[] sentences) {
+        int wordCount = 0;
+        for(String str : sentences) {
+            int count = str.split(" ").length;
+            if(count > wordCount) {
+                wordCount = count;
+            }
+        }
+        return wordCount;
+    }
 }
