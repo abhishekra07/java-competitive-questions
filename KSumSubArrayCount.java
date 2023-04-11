5464aed5f2870e81874e9240d0f7cc0bc7572e11
abhishekra07 <abhira719@gmail.com>
2023-04-11 15:20:20 +0530

    Update KSumSubArrayCount.java

--- KSumSubArrayCount.java
+++ KSumSubArrayCount.java
@@ -1,5 +1,22 @@
+import java.util.HashMap;
+
 public class KSumSubArrayCount {
     public static void main(String[] args) {
-        
+        subarraySum(new int[]{3, 9, -2, 4, 1, -7, 2, 6, -5, 8, -3, -7, 6, 2, 1}, 5);
+    }
+
+    public static void subarraySum(int[] arr, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+        int sum = 0;
+        int ans = 0;
+        for(int i : arr) {
+            sum += i;
+            if(map.containsKey(sum - target))  {
+                ans += map.get(sum - target);
+            }
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+        }
+        System.out.println("asn " + ans);
     }
 }
