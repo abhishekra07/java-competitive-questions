00848281ba6349cea9c8e5b27139027516262f7a
abhishekra07 <abhira719@gmail.com>
2023-04-11 14:10:27 +0530

    Update FirstNonRepeatingCharacter.java

--- FirstNonRepeatingCharacter.java
+++ FirstNonRepeatingCharacter.java
@@ -1,10 +1,36 @@
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class FirstNonRepeatingCharacter {
     public static void main(String[] args) {
-        String str = "aabbde";
-        System.out.println(findNonRepeatingChar(str))
+        String str = "aabbdde";
+        System.out.println(findNonRepeatingChar1(str));
     }
 
+    public static char findNonRepeatingChar1(String str) {
+        // Time Complexity: O(N2)
+        int index = -1;
+        char fnc = ' ';
+        for (char i : str.toCharArray()) {
+            if (str.indexOf(i) == str.lastIndexOf(i)) {
+                fnc = i;
+                break;
+            }
+            else {
+                index += 1;
+            }
+        }
+        return fnc;
+    }
     public static char findNonRepeatingChar(String str) {
-        return 'a';
+        Map<Character, Integer> counterMap = new LinkedHashMap<>();
+        for(char ch : str.toCharArray()) {
+            counterMap.put(ch, counterMap.getOrDefault(ch, 0) + 1);
+        }
+        for(Map.Entry<Character, Integer> entry : counterMap.entrySet()) {
+            if(entry.getValue() == 1)
+                return entry.getKey();
+        }
+        throw new RuntimeException("didn't find any non repeating character");
     }
 }
