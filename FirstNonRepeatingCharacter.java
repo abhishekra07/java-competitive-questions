c552b40c3597437473ebd374137a577e00078d2c
abhishekra07 <abhira719@gmail.com>
2023-04-11 13:58:32 +0530

    Update FirstNonRepeatingCharacter.java

--- FirstNonRepeatingCharacter.java
+++ FirstNonRepeatingCharacter.java
@@ -1,5 +1,10 @@
 public class FirstNonRepeatingCharacter {
     public static void main(String[] args) {
-        
+        String str = "aabbde";
+        System.out.println(findNonRepeatingChar(str))
+    }
+
+    public static char findNonRepeatingChar(String str) {
+        return 'a';
     }
 }
