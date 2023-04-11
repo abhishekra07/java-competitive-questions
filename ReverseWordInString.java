aa83745e64419b243a7d549d92b0ff51ce4692fb
abhishekra07 <abhira719@gmail.com>
2023-04-11 17:24:44 +0530

    Update ReverseWordInString.java

--- ReverseWordInString.java
+++ ReverseWordInString.java
@@ -2,6 +2,12 @@ public class ReverseWordInString {
     public static void main(String[] args) {
         System.out.println(reverseWords("the sky is blue"));
     }
+
+    /**
+     * Reverse words in the string
+     * @param s
+     * @return
+     */
     public static String reverseWords(String s) {
         s = s.trim();
         String[] splitted = s.split("\\s+");
