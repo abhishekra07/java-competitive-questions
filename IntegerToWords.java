2f3efd9514029255aa0b6a7add1e6c584b09b561
abhishekra07 <abhira719@gmail.com>
2023-04-12 16:57:12 +0530

    Update IntegerToWords.java

--- IntegerToWords.java
+++ IntegerToWords.java
@@ -4,7 +4,9 @@ public class IntegerToWords {
     private static final String[] THOUSANDS = {"", "Thousand", "Million", "Billion"};
 
     public static void main(String[] args) {
-        System.out.println(numberToWords(123));
+//        System.out.println(numberToWords(123));
+//        System.out.println(new Solution().numberToWords(123));
+        System.out.println(new Solution2().numberToWords(123));
     }
 
     public static String numberToWords(int num) {
@@ -32,5 +34,58 @@ public class IntegerToWords {
             return TENS[num / 10] + " " + helper(num % 10);
         else
             return LESS_THAN_20[num / 100] + " Hundred " + helper(num % 100);
+    }
+}
+
+/**
+ *
+ */
+class Solution {
+    private final String[] belowTen = new String[] {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"};
+    private final String[] belowTwenty = new String[] {"Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
+    private final String[] belowHundred = new String[] {"", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
+
+    public String numberToWords(int num) {
+        if (num == 0) return "Zero";
+        return helper(num);
+    }
+
+    private String helper(int num) {
+        String result = new String();
+        if (num < 10) result = belowTen[num];
+        else if (num < 20) result = belowTwenty[num -10];
+        else if (num < 100) result = belowHundred[num/10] + " " + helper(num % 10);
+        else if (num < 1000) result = helper(num/100) + " Hundred " +  helper(num % 100);
+        else if (num < 1000000) result = helper(num/1000) + " Thousand " +  helper(num % 1000);
+        else if (num < 1000000000) result = helper(num/1000000) + " Million " +  helper(num % 1000000);
+        else result = helper(num/1000000000) + " Billion " + helper(num % 1000000000);
+        return result.trim();
+    }
+}
+
+/**
+ * Another Solution
+ * Best one
+ */
+class Solution2 {
+    String[] tens = {"", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
+    String[] ones = {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven",
+            "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
+
+    public String numberToWords(int num) {
+        if (num == 0)
+            return "Zero";
+
+        return helper(num).trim();
+    }
+
+    private String helper(int num) {
+
+        if (num >= 1000000000) return (helper(num / 1000000000) + " Billion " + helper(num % 1000000000));
+        if (num >= 1000000) return (helper(num / 1000000) + " Million " + helper(num % 1000000));
+        if (num >= 1000) return (helper(num / 1000) + " Thousand " + helper(num % 1000));
+        if (num >= 100) return (helper(num / 100) + " Hundred " + helper((num % 100))).trim();
+        if (num >= 20) return (tens[num / 10] + " " + helper(num % 10)).trim();
+        return ones[num];
     }
 }
