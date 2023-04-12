9179e6d7593efc2bb496a1e84170167fa2ba3f45
abhishekra07 <abhira719@gmail.com>
2023-04-12 14:54:13 +0530

    Update IntegerToRoman.java

--- IntegerToRoman.java
+++ IntegerToRoman.java
@@ -1,5 +1,28 @@
 public class IntegerToRoman {
     public static void main(String[] args) {
-        
+        System.out.println(intToRoman(1904));
+    }
+
+    public static String intToRoman(int num) {
+        String ones[] = {"","I","II","III","IV","V","VI","VII","VIII","IX"};
+        String tens[] = {"","X","XX","XXX","XL","L","LX","LXX","LXXX","XC"};
+        String hrns[] = {"","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"};
+        String ths[]={"","M","MM","MMM"};
+
+        return ths[num/1000] + hrns[(num%1000)/100] + tens[(num%100)/10] + ones[num%10];
+    }
+
+    public static String intToRoman2(int num) {
+        String roman = "";
+        int[] storeInt = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        String[] storeRoman = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        for (int i = 0; i < storeInt.length; i++) {
+            while (num >= storeInt[i]) {
+                roman += storeRoman[i];
+                num -= storeInt[i];
+            }
+        }
+
+        return roman;
     }
 }
