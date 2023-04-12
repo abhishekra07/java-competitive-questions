5076f6453763feab9f994dd164d9a609f2ea261f
abhishekra07 <abhira719@gmail.com>
2023-04-12 10:38:20 +0530

    Update LongestCommonPrefix.java

--- LongestCommonPrefix.java
+++ LongestCommonPrefix.java
@@ -12,16 +12,13 @@ public class LongestCommonPrefix {
         int n = arr.length;
         // take temp_prefix string and assign first element of arr : a.
         String result = arr[0];
-        System.out.println("result "+ result);
 
         // Iterate for rest of element in string.
         for(int i = 1; i < n; i++){
             // .indexOf() return index of that substring from string.
-            System.out.println(arr[i].indexOf(result));
             while(arr[i].indexOf(result) != 0){
                 // update matched substring prefx
                 result = result.substring(0, result.length()-1);
-                System.out.println("result :- "+ result);
                 // check for empty case. direct return if true..
                 if(result.isEmpty()){
                     return "-1";
