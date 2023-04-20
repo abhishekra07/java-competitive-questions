5ef2f80c535164a2ee6461784238817ceed7cdc7
abhishekra07 <abhira719@gmail.com>
2023-04-20 11:28:14 +0530

    Update JewelsAndStones.java

--- JewelsAndStones.java
+++ JewelsAndStones.java
@@ -1,5 +1,30 @@
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * You're given strings jewels representing the types of stones that are jewels, and stones representing the stones you have. Each character in stones is a type of stone you have. You want to know how many of the stones you have are also jewels.
+ *
+ * Letters are case sensitive, so "a" is considered a different type of stone from "A".
+ *
+ * Example 1:
+ *
+ * Input: jewels = "aA", stones = "aAAbbbb"
+ * Output: 3
+ */
 public class JewelsAndStones {
     public static void main(String[] args) {
-        
+        System.out.println(numJewelsInStones("aA", "aAAbbbb"));
+    }
+
+    public static int numJewelsInStones(String jewels, String stones) {
+        int res = 0;
+        for(int i=0;i<jewels.length();i++) {
+            for(int j=0;j<stones.length();j++) {
+                if(jewels.charAt(i) == stones.charAt(j)) {
+                    res++;
+                }
+            }
+        }
+        return res;
     }
 }
